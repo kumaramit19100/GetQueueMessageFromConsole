ca7aa5e01cb9ec612f7d4f17bacb4734ad527b0c
amit kumar <kumaramit19100@gmail.com>
2022-05-04 16:54:47 +0530

    Initial Commit

---
+++ GetQueueMessageFromConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CloudeStorageAccount = Microsoft.WindowsAzure.Storage.CloudStorageAccount;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace GetQueueMessageFromConsole
+{
+    class Program
+    {
+        public static string ConnectionString = "DefaultEndpointsProtocol=https;AccountName=akstaccount;AccountKey=gkGSx+sQsRaUtEU9YAl5cxogSchyKpV7qHl8pkJ+rhuA+yiwllI+0C249yF7cRTu6JrAGX5Dr3Uo8i7rGjQqbA==;EndpointSuffix=core.windows.net";
+        public static string QueueName = "task-1";
+        static void Main(string[] args)
+        {
+            GetMessage();
+            Console.ReadKey();
+        }
+
+        public static void GetMessage()
+        {
+            CloudeStorageAccount cls = CloudeStorageAccount.Parse(ConnectionString);
+            CloudQueueClient queueClient = cls.CreateCloudQueueClient();
+            CloudQueue cloudQueue = queueClient.GetQueueReference(QueueName);
+            CloudQueueMessage queueMessage = cloudQueue.GetMessage();
+            if (queueMessage != null)
+            {
+                Console.WriteLine(queueMessage.AsString);
+            }
+            else
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+For run this program Install nuget Package, 1: WindowsAzure.Storage, 2: Microsoft.Azure.Webjobs.Extension.Storage 
